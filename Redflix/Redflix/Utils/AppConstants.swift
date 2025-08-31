350c2bc989c460841064155cf9f0a26691a24b4d
wtao <wtao@redfast.com>
2025-08-31 10:44:19 -0700

    hide secret

--- Redflix/Redflix/Utils/AppConstants.swift
+++ Redflix/Redflix/Utils/AppConstants.swift
@@ -11,7 +11,8 @@ enum AppConstants {
     static let collectionId = "635c3e79a327a596b2d7a7cd"
     
     #warning("Please contact the customer support to get the appId, userId and webflowBearerToken.")
-    static let appId = "2b40fc8a-75fe-436e-afa7-e2879392566c"
-    static let userId = "123-ios"
-    static let webflowBearerToken = "c405ed357f5046964ad69f1b5f45df7da5a03913af05cf685612a724bd5e48ed"
+    static let appId = "YOUR_APP_ID"
+    static let appId = "YOUR_APP_ID"
+    static let userId = "USER_ID"
+    static let webflowBearerToken = "YOUR_BEARER_TOKEN"
 }
