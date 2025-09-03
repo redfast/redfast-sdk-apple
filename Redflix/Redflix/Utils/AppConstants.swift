238f0d6e1b7b185be01248a928d29086ae33a6ed
wtao-redfast <53849964+wtao-redfast@users.noreply.github.com>
2025-09-03 09:42:39 -0700

    cleanup code (#14)
    * cleanup code
    
    * hide secret
    
    * extra clean

--- Redflix/Redflix/Utils/AppConstants.swift
+++ Redflix/Redflix/Utils/AppConstants.swift
@@ -11,8 +11,7 @@ enum AppConstants {
     static let collectionId = "635c3e79a327a596b2d7a7cd"
     
     #warning("Please contact the customer support to get the appId, userId and webflowBearerToken.")
-    static let appId = "REMOVED"
-//    static let appId =  "REMOVED" // lee
-    static let userId = "123-ios"
-    static let webflowBearerToken = "REMOVED"
+    static let appId = "YOUR_APP_ID"
+    static let userId = "USER_ID"
+    static let webflowBearerToken = "YOUR_BEARER_TOKEN"
 }
