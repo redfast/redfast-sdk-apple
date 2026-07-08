ab25e0bdda713745fc938674ae31af3b1972b581
Sebastian Bonilla <sbonilla@recurly.com>
2026-07-08 10:52:15 -0500

    RE-1031 add final versions for package flavors

--- Package.swift
+++ Package.swift
@@ -24,23 +24,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "redfast_core",
-            url: "https://github.com/recurly/redfast-sdk-apple/releases/download/3.0.0-pre-release/redfast-core-3.0.0.xcframework.zip",
-            checksum: "666cd0769f3b848fb4c065820bfe818b6db0c293609142c1db6efccbb45c2a73"
+            url: "https://github.com/recurly/redfast-sdk-apple/releases/download/3.0.0/redfast-core-3.0.0.xcframework.zip",
+            checksum: "a36f1904a2cdbbf1b5b68d3ba274beb741d5ff7dcaa3057812116973fa70904e"
         ),
         .binaryTarget(
             name: "redfast_ui",
-            url: "https://github.com/recurly/redfast-sdk-apple/releases/download/3.0.0-pre-release/redfast-ui-3.0.0.xcframework.zip",
-            checksum: "d4cfa9449b0b4c94776e46ae0bba7aabb07205f7f5e69bc7193b0f6866946ec2"
+            url: "https://github.com/recurly/redfast-sdk-apple/releases/download/3.0.0/redfast-ui-3.0.0.xcframework.zip",
+            checksum: "1f994ed55b29a5d1848fe6530f22d85695b53ef072e8011fa07f608f43f0ca46"
         ),
         .binaryTarget(
             name: "redfast_ui_iap",
-            url: "https://github.com/recurly/redfast-sdk-apple/releases/download/3.0.0-pre-release/redfast-ui-iap-3.0.0.xcframework.zip",
-            checksum: "8604bf7134011c01178004bd42ad9ca61e3d9f3d61b6f15cdbf152cc9836aef0"
+            url: "https://github.com/recurly/redfast-sdk-apple/releases/download/3.0.0/redfast-ui-iap-3.0.0.xcframework.zip",
+            checksum: "431f5ffe1f76e155ca81a7b0b498d3839f2457bb2537bb9b3d9660dbf15c118b"
         ),
         .binaryTarget(
             name: "redfast_ui_push",
-            url: "https://github.com/recurly/redfast-sdk-apple/releases/download/3.0.0-pre-release/redfast-ui-push-3.0.0.xcframework.zip",
-            checksum: "dd7dc08d40e1bfbc15782ccf26d82a33330b107e16b3fdc7eddd6bfd3753c0b4"
+            url: "https://github.com/recurly/redfast-sdk-apple/releases/download/3.0.0/redfast-ui-push-3.0.0.xcframework.zip",
+            checksum: "ab2f1e4affa825b097fb5ac2d20da9e654ca1f711503950101d4a0ba42021cb6"
         ),
     ]
 )
